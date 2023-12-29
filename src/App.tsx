9ffefc543112e5ff1f4f48eb3a1d21d9ea7bd22f
AbdallahAyed <abdalla.ayed.2010@gmail.com>
2023-12-29 15:31:38 -0800

    Add products table

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
-import { useEffect } from "react";
-import Home from "./Home";
 import Sidebar from "./components/Sidebar";
+
 import { Outlet } from "react-router-dom";
 
 function App() {
-  useEffect(() => {
-    console.log(Outlet);
-  });
-
   return (
     <div id="app">
       <div className="container">
         <Sidebar />
-        {!(<Outlet />) ? <Home /> : <Outlet />}
+        <Outlet />
       </div>
     </div>
   );
--- src/Products.tsx
+++ src/Products.tsx
@@ -1,7 +1,61 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+  operations: React.ReactNode;
+};
+
 function Products() {
+  const [data, setData] = useState<any>();
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/products")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  console.log(data);
+
   return (
     <div className="products">
-      <h1>Products</h1>
+      <h1>Products Home</h1>
+
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Image</th>
+            <th>Operations</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((product: Product) => (
+            <tr key={product.id}>
+              <td>{product.id}</td>
+              <td>{product.title.slice(0, 12)}</td>
+              <td>{product.price}</td>
+              <td>
+                <img src={product.image} alt="img" />
+              </td>
+              <td>
+                <button>View</button>
+                <button>Edit</button>
+                <button>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
