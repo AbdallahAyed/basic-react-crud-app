8e97535e6b4e48264637cc2c72a7b01129ef7d69
AbdallahAyed <abdalla.ayed.2010@gmail.com>
2023-12-29 10:17:27 -0800

    Add Sidebar and react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { Link } from "react-router-dom";
+
 function App() {
   return (
-    <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div id="app">
+      <div className="container">
+        <aside>
+          <Link
+            to={"/"}
+            style={{
+              margin: "1rem",
+              textDecoration: "none",
+              color: "blue",
+            }}
+          >
+            <p>Logo</p>
+          </Link>
+          <ul>
+            <li>Get All Products</li>
+            <li>Get All Categories</li>
+          </ul>
+        </aside>
+      </div>
+    </div>
   );
 }
 
