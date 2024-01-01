14b9521637b69f27649dbeb218cb7b2e56df20c6
AbdallahAyed <abdalla.ayed.2010@gmail.com>
2023-12-31 18:29:08 -0800

    Add deleting request functionality

--- src/Products.tsx
+++ src/Products.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 export type ProductType = {
   id: number;
@@ -19,6 +20,10 @@ function Products() {
   const [data, setData] = useState<any>();
 
   useEffect(() => {
+    getAllProducts();
+  }, []);
+
+  const getAllProducts = () => {
     axios
       .get("http://localhost:3000/products")
       .then((res) => {
@@ -27,19 +32,49 @@ function Products() {
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  };
+
+  const deleteProduct = (product: ProductType) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
+        axios
+          .delete(`http://localhost:3000/products/${product.id}`)
+          .then((response) => {
+            console.log(`Deleted post with ID ${product.id}`, response);
+            getAllProducts();
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      }
+    });
+  };
 
   return (
     <div className="products">
       <h1>Products Home</h1>
 
+      <button className="add-product">Add new product</button>
       <table>
         <thead>
           <tr>
             <th>ID</th>
             <th>Title</th>
             <th>Price</th>
-            <th>Image</th>
+            <th>Category</th>
             <th>Operations</th>
           </tr>
         </thead>
@@ -49,15 +84,13 @@ function Products() {
               <td>{product.id}</td>
               <td>{product.title.slice(0, 12)}</td>
               <td>{product.price}</td>
-              <td>
-                <img src={product.image} alt="img" />
-              </td>
+              <td>{product.category.toUpperCase()}</td>
               <td>
                 <Link to={`/products/${product.id}`}>
                   <button>View</button>
                 </Link>
                 <button>Edit</button>
-                <button>Delete</button>
+                <button onClick={() => deleteProduct(product)}>Delete</button>
               </td>
             </tr>
           ))}
